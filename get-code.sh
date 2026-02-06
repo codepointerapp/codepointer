93a1139d7804dafd110311e88c781dc7b1903dd0
Diego Iastrubni <diegoiast@gmail.com>
2026-02-06 18:22:45 +0200

    get-code.sh: support older shells
    I am using alpine linux, which by default does not have bash (I am actually using busybox!).
    
    So its shell is very limited (even `vi` comes from busybox, very epic IMHO`). So,
    Instead of using some bashism - port this code to older more limited shells.

--- get-code.sh
+++ get-code.sh
@@ -1,52 +1,47 @@
-#! /bin/bash
-
-# Assume you are on a trip, and want to work on the code of the project.
-# Before leaving, run this script, and the configure with QTEDIT4_WORK_OFFLINE=on
-#
-# Now you should be able to work offline (the CMake stuff will not query online servers.
-#
-# When you are done - for each of the repos you modified, create a new branch,
-# and push and create PRs.
+#!/bin/sh
 
 set -e
 
 clone_or_update_repo() {
-    local owner=$1
-    local repo=$2
-    local release=$3
-    local target_dir="lib/$repo"
+    owner="$1"
+    repo="$2"
+    release="$3"
+    target_dir="lib/$repo"
 
     if [ -d "$target_dir" ]; then
-        echo -n "Updating $target_dir"
+        printf "Updating %s" "$target_dir"
         (cd "$target_dir" && git fetch -q origin)
     else
-        echo -n "Cloning $target_dir (gh:$owner/$repo)"
+        printf "Cloning %s (gh:%s/%s)" "$target_dir" "$owner" "$repo"
         git clone -q "https://github.com/$owner/$repo" "$target_dir"
-        cd $target_dir
-        git remote add github "git@github.com:$owner/$repo"
-        cd - > /dev/null
+        (
+            cd "$target_dir"
+            git remote add github "git@github.com:$owner/$repo"
+        )
     fi
-    echo -n "."
+    printf "."
 
-    echo -n " Updating to $release"
+    printf " Updating to %s" "$release"
     (
         cd "$target_dir"
-        if ! git checkout -q $release 2>/dev/null &&
-           ! git checkout -q origin/$release 2>/dev/null; then
+        if ! git checkout -q "$release" 2>/dev/null &&
+           ! git checkout -q "origin/$release" 2>/dev/null; then
             echo "Failed to checkout $release for $repo. Falling back to master/main."
-            #git checkout -q master || git checkout -q main
-            exit -1
+            exit 1
         fi
     )
     echo "."
 }
 
-while read -r line; do
-    if [[ $line =~ CPMAddPackage\(\"gh:([^/]+)/([^#]+)#([^\"]+) ]]; then
-        owner=${BASH_REMATCH[1]}
-        repo=${BASH_REMATCH[2]}
-        release=${BASH_REMATCH[3]}
+grep 'CPMAddPackage' CMakeLists.txt | while IFS= read -r line; do
+    parsed=$(echo "$line" | sed -n \
+        's/.*CPMAddPackage("gh:\([^/]*\)\/\([^#]*\)#\([^"]*\)".*/\1 \2 \3/p')
+
+    if [ -n "$parsed" ]; then
+        owner=$(echo "$parsed" | awk '{print $1}')
+        repo=$(echo "$parsed"  | awk '{print $2}')
+        release=$(echo "$parsed" | awk '{print $3}')
 
         clone_or_update_repo "$owner" "$repo" "$release"
     fi
-done < <(grep CPMAddPackage CMakeLists.txt)
+done
